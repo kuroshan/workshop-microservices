fa89734443793281939e8fc5393f3539c4fd124a
Americo Sierra <eltrogloditakia@gmail.com>
2020-06-14 23:10:52 -0500

    10

---
+++ session3/infrastructure/zuul-server/src/test/java/com/kuroshan/workshop/ms/hr/zuul/server/ZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuroshan.workshop.ms.hr.zuul.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- session3/microservices/ms-employees/src/main/java/com/kuroshan/workshop/ms/hr/employees/config/AreasClientRest.java
+++ session3/microservices/ms-employees/src/main/java/com/kuroshan/workshop/ms/hr/employees/config/AreasClientRest.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-hr-areas", path = "/ms-hr-areas/v1")
+@FeignClient(name = "ms-hr-areas", path = "/v1")
 public interface AreasClientRest {
 
   @GetMapping(value = "/departments/custom/{id}")
