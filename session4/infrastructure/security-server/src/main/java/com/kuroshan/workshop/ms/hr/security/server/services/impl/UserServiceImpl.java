16e991ac2962ea9e815b5881f0c51f9b31f5376b
Americo Sierra <eltrogloditakia@gmail.com>
2020-06-28 16:53:10 -0500

    13

--- session4/infrastructure/security-server/src/main/java/com/kuroshan/workshop/ms/hr/security/server/services/impl/UserServiceImpl.java
+++ session4/infrastructure/security-server/src/main/java/com/kuroshan/workshop/ms/hr/security/server/services/impl/UserServiceImpl.java
@@ -16,7 +16,7 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @Service
-public class UserService implements UserDetailsService {
+public class UserServiceImpl implements UserDetailsService {
 
   @Autowired
   private UsersClientRest usersClientRest;
