0e504b005ce00e7d0b5fa201940cf5a6beb4e0e0
Americo Sierra <eltrogloditakia@gmail.com>
2020-06-29 16:57:10 -0500

    13

--- session4/infrastructure/security-server/src/main/java/com/kuroshan/workshop/ms/hr/security/server/SecurityServerApplication.java
+++ session4/infrastructure/security-server/src/main/java/com/kuroshan/workshop/ms/hr/security/server/SecurityServerApplication.java
@@ -1,17 +1,32 @@
 package com.kuroshan.workshop.ms.hr.security.server;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@Slf4j
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
-public class SecurityServerApplication {
+public class SecurityServerApplication implements CommandLineRunner {
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		for (int i = 0; i < 2; i++) {
+			log.info(passwordEncoder.encode("123456"));
+		}
 	}
 
 }
