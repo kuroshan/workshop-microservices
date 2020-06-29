c36f7a5dc84dfcb45c48a2f7f970ec06ea48f21b
Americo Sierra <eltrogloditakia@gmail.com>
2020-06-29 17:03:47 -0500

    13

--- session4/microservices/ms-users/src/main/resources/import.sql
+++ session4/microservices/ms-users/src/main/resources/import.sql
@@ -1,5 +1,6 @@
-insert into users (username, password, email, enabled) values ('eargume', '123456', 'eargume@kuroshan.com', true)
-insert into users (username, password, email, enabled) values ('klozano', '123456', 'klozano@kuroshan.com', true)
+-- password: 123456
+insert into users (username, password, email, enabled) values ('user', '$2a$10$RJorbfyOiZskAYj3D3nvZexJ8U9YCMtrPoYoQR9TJPrypCluv897C', 'user@kuroshan.com', true)
+insert into users (username, password, email, enabled) values ('admin', '$2a$10$g23HCEkssqbtG5XOZ39NLOx6iAxyJRkgcyPmnJ7TM0LdwE5E7ePYu', 'admin@kuroshan.com', true)
 
 insert into roles (name) values ('ROLE_USER');
 insert into roles (name) values ('ROLE_ADMIN');
