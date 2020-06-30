d11c3dbc2131cae631e2c24083c5169092c2444f
Americo Sierra <eltrogloditakia@gmail.com>
2020-06-30 18:03:39 -0500

    13

--- session4/infrastructure/zuul-server/src/main/java/com/kuroshan/workshop/ms/hr/zuul/server/config/ResourceServerConfig.java
+++ session4/infrastructure/zuul-server/src/main/java/com/kuroshan/workshop/ms/hr/zuul/server/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.kuroshan.workshop.ms.hr.zuul.server.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -9,10 +12,20 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+  @Value("${security.oauth.jwt.key}")
+  private String jwtKey;
 
   @Override
   public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -26,7 +39,30 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         .antMatchers("/api/hr/security/oauth/**").permitAll()
         .antMatchers(HttpMethod.GET, "/api/hr/areas/departments").permitAll()
         .antMatchers(HttpMethod.GET, "/api/hr/users/users").hasRole("ADMIN")
-        .anyRequest().authenticated();
+        .anyRequest().authenticated()
+        .and()
+        .cors().configurationSource(corsConfigurationSource());
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration corsConfig = new CorsConfiguration();
+    corsConfig.addAllowedOrigin("*");
+    corsConfig.addAllowedMethod(HttpMethod.GET);
+    corsConfig.setAllowCredentials(true);
+    corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", corsConfig);
+
+    return source;
+  }
+
+  @Bean
+  public FilterRegistrationBean<CorsFilter> corsFilter() {
+    FilterRegistrationBean<CorsFilter> filter = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+    filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    return filter;
   }
 
   @Bean
@@ -37,7 +73,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
   @Bean
   public JwtAccessTokenConverter accessTokenConverter() {
     JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-    tokenConverter.setSigningKey("token_secrect_key");
+    tokenConverter.setSigningKey(this.jwtKey);
     return tokenConverter;
   }
 
